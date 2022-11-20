82fdc68f89967045463b14be4ce2be22b74a7dab
AnnaKrivoruchk <Annakr98@mail.ru>
2022-11-20 22:20:14 +0300

    Modul_16.9

---
+++ Modul16Unit9task16.9.2.py
@@ -0,0 +1,16 @@
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    def __str__(self):
+        return f'Rectangle: {self.x}, {self.y}, {self.width}, {self.height}.'
+    def get_area(self):
+        return self.width * self.height
+
+rect_1 = Rectangle(5, 10, 50, 100)
+rect_2 = Rectangle(10, 20, 40, 200)
+
+print(rect_1, rect_1.get_area())
+print(rect_2, rect_2.get_area())
